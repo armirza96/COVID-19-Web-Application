ad3fe2d2d43c1f6c05b83413146132cb7e654b9d
kvmn2000 <72999480+kvmn2000@users.noreply.github.com>
2021-08-11 18:21:01 -0400

    healthcaareWorker

---
+++ js/healthcareWorker/healthecareWorkerAdd.js
@@ -0,0 +1,27 @@
+function addPatient() {
+    $('.modal').modal('toggle')
+    let dataToSend = {PAGE: 'addPatient'};
+    $('form :input').each(function(){
+        const name = $(this).attr('name');
+        const value = $(this).val();
+        dataToSend[name] = value;
+    });
+    dataToSend['IS_CITIZEN'] = $('#inputCitizen').val();
+    dataToSend['AGE_GROUP'] = $('#ageGroup').val();
+    console.log(dataToSend);
+    doAjaxCall(dataToSend, {callback: 'showAddedPatientAlert', parentEl: 'body', type: 'UI_UPDATE'}, 'POST');
+}
+
+function showAddedPatientAlert(parentEl, data) {
+  if(data.RESULT == 1) {
+    $('.alert-success').text(data.MESSAGE + '. ' + 'Redirecting in 3 seconds');
+    $('.alert-success').show();
+
+    setTimeout(function(){
+      window.location.href = `patient.php?PATIENT_ID=${data.ID}`;
+    }, 3000);
+  } else {
+    $('.alert-danger').text(data.MESSAGE);
+    $('.alert-success').show()
+  }
+}
---
+++ php/healthcareWorker/get/getHealthcareWorkers.php
@@ -0,0 +1,4 @@
+<?php
+require_once("././getter.php");
+
+$data = getData("healthcareWorker/get/getHealthcareWorkers.txt");
